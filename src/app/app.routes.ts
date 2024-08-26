2a3d25768d94170186d4b2af45b989807fa3b658
satishrkt <satishrkt10@gmail.com>
2024-08-26 11:56:03 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './main-component/home/home.component';
+import { PageNotFoundComponent } from './shared-component/page-not-found/page-not-found.component';
+import { AboutComponent } from './main-component/about/about.component';
+import { EventComponent } from './main-component/event/event.component';
+import { ResourcesComponent } from './main-component/resources/resources.component';
+import { ContactComponent } from './main-component/contact/contact.component';
+import { PrivateComponent } from './main-component/private/private.component';
+import { CorporateComponent } from './main-component/corporate/corporate.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+    { path: 'event', component: EventComponent },
+    { path: 'resourecs', component: ResourcesComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'private', component: PrivateComponent },
+    { path: 'corporate', component: CorporateComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
---
+++ src/app/main-component/corporate/corporate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corporate',
+  standalone: true,
+  imports: [],
+  templateUrl: './corporate.component.html',
+  styleUrl: './corporate.component.css'
+})
+export class CorporateComponent {
+
+}
